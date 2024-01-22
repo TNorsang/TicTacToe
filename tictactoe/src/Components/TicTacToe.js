7dfd33321c6b29948fd3df1f6c1f678ce872aabf
Norsang Nyandak <TNorsang>
2024-01-22 12:44:47 -0500

    created components

---
+++ tictactoe/src/Components/TicTacToe.js
@@ -0,0 +1,13 @@
+import "./TicTacToe.css";
+
+export default function TicTacToe() {
+  return (
+    <div className="container">
+      <div className="title">
+        <h1>
+          TicTacToe - <span>Norsang Nyandak</span>
+        </h1>
+      </div>
+    </div>
+  );
+}
